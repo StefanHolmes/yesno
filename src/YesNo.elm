4e7bcc9d8ab458fa32b64d19b590a9228be3d5f3
Stefan Holmes <stefan.c.holmes@gmail.com>
2020-09-08 21:10:45 +0100

    Mostly broken

--- src/YesNo.elm
+++ src/YesNo.elm
@@ -1,24 +1,58 @@
 module YesNo exposing (main)
 
+{- This is an "I'm trying to learn something difficult" first project.
+
+   Intended to be deployed by Netlify to shouldigotothebeach.com
+-}
+
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Prompts exposing (..)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
 
 
 type alias Model =
     String
 
 
-
-{- This is an "I'm trying to learn something difficult" first project.
-
-   Intended to be deployed by Netlify to shouldigotothebeach.com
--}
-
-
-initialModel : Model
-initialModel =
+init : Model
+init =
     "Is the weather really nice?"
+
+
+
+-- UPDATE
+
+
+type Msg
+    = AnswerYes
+    | AnswerNo
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AnswerNo ->
+            "Bzzzzt no"
+
+        AnswerYes ->
+            "Whoop yes"
+
+
+
+-- VIEW
 
 
 view : Model -> Html msg
@@ -28,13 +62,8 @@ view model =
         , div [ id "question" ]
             [ h2 [] [ text model ]
             , div [ id "answers" ]
-                [ div [ id "yes" ] [ p [] [ text "Yes" ] ]
-                , div [ id "no" ] [ p [] [ text "No" ] ]
+                [ div [ onClick AnswerYes id "yes" ] [ p [] [ text "Yes" ] ]
+                , div [ onClick AnswerNo id "no" ] [ p [] [ text "No" ] ]
                 ]
             ]
         ]
-
-
-main : Html msg
-main =
-    view initialModel
